60c545bb6e6fe544c9902dff36c294a5be1dc6c2
Fahmi Fauzi <fahmi.fauzi@ifg-life.id>
2024-12-07 18:04:49 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:5000/:path*",
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
